19ee0c201b2081c305e0d07f883d50de6a8adb9b
jyodai <tdkdtk5011@gmail.com>
2022-12-18 20:35:44 +0900

    vimのセットアップスクリプト

---
+++ vimInstall.sh
@@ -0,0 +1,8 @@
+add-apt-repository ppa:jonathonf/vim
+apt update
+apt install vim
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ ! -d $HOME/.vim/plugged ]; then
+    mkdir ~/.vim/plugged
+fi
