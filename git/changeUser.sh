ebf9000706b67a2f919a00de9e2027e8202bc4c1
$GITHUB_USER <$GITHUB_MAIL>
2022-12-22 02:26:51 +0900

    リポジトリの階層を指定可能にする

--- git/changeUser.sh
+++ git/changeUser.sh
@@ -13,14 +13,14 @@ main() {
 
 changeCompanyUser() {
     echo "change company user : $GITHUB_COMPANY_USER"
-    sed -i "s/\(GITHUB_USER=\).*$/\1$GITHUB_COMPANY_USER/" $HOME/.env
-    sed -i "s/\(GITHUB_MAIL=\).*$/\1$GITHUB_COMPANY_MAIL/" $HOME/.env
+    sed -i "s/\(GITHUB_USER=\).*$/\1$GITHUB_COMPANY_USER/" $SETTING_PATH/.env
+    sed -i "s/\(GITHUB_MAIL=\).*$/\1$GITHUB_COMPANY_MAIL/" $SETTING_PATH/.env
 }
 
 changePrivateUser() {
     echo "change private user : $GITHUB_PRIVATE_USER"
-    sed -i "s/\(GITHUB_USER=\).*$/\1$GITHUB_PRIVATE_USER/" $HOME/.env
-    sed -i "s/\(GITHUB_MAIL=\).*$/\1$GITHUB_PRIVATE_MAIL/" $HOME/.env
+    sed -i "s/\(GITHUB_USER=\).*$/\1$GITHUB_PRIVATE_USER/" $SETTING_PATH/.env
+    sed -i "s/\(GITHUB_MAIL=\).*$/\1$GITHUB_PRIVATE_MAIL/" $SETTING_PATH/.env
 }
 
 
