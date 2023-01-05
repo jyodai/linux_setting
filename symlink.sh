83a103c51f20800566c597c19a428984b75c0699
jyodai <tdkdtk5011@gmail.com>
2023-01-06 07:45:07 +0900

    シンボリックリンクの削除を可能にする

--- symlink.sh
+++ symlink.sh
@@ -9,20 +9,45 @@ filePaths=(
     ".gitconfig.user"
 )
 
-for filePath in "${filePaths[@]}"
-do
-    if [ -L $HOME/"$filePath" ]; then
-        echo "link already exists : $HOME/$filePath"
-        continue
-    fi
+main() {
+    case "$1" in
+      "u")
+        execUnlink
+        ;;
+      *)
+        execSymlink
+    esac
+}
 
-    if [ -f $HOME/"$filePath" ]; then
-        backupName=$HOME/"$filePath"'_bk'`date +%Y%m%d`
-        mv $HOME/"$filePath" $backupName
-        echo "backup : $backupName"
-    fi
+execSymlink() {
+    for filePath in "${filePaths[@]}"
+    do
+        if [ -L $HOME/"$filePath" ]; then
+            echo "link already exists : $HOME/$filePath"
+            continue
+        fi
 
-    echo "symlink : $HOME/$filePath"
-    ln -s $HOME/linux_setting/"$filePath" $HOME/"$filePath"
-done
+        if [ -f $HOME/"$filePath" ]; then
+            backupName=$HOME/"$filePath"'_bk'`date +%Y%m%d`
+            mv $HOME/"$filePath" $backupName
+            echo "backup : $backupName"
+        fi
 
+        echo "symlink : $HOME/$filePath"
+        ln -s $HOME/linux_setting/"$filePath" $HOME/"$filePath"
+    done
+}
+
+execUnlink() {
+    for filePath in "${filePaths[@]}"
+    do
+        if [ ! -L $HOME/"$filePath" ]; then
+            continue
+        fi
+
+        unlink $HOME/"$filePath"
+        echo "unlink : $HOME/$filePath"
+    done
+}
+
+main $1
