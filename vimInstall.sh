b91b52f0c964da3364ca51ffced226fd791af5f5
jyodai <tdkdtk5011@gmail.com>
2023-01-06 07:02:34 +0900

    リファクタリング

--- vimInstall.sh
+++ vimInstall.sh
@@ -1,13 +1,22 @@
+#!/bin/bash
+
 add-apt-repository ppa:jonathonf/vim
 apt update
 apt install vim
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-if [ ! -d $HOME/.vim/plugged ]; then
-    mkdir ~/.vim/plugged
-fi
+dirPaths=(
+    ".vim/plugin"
+    ".vim/plugged"
+    ".vim/project"
+)
 
-if [ ! -d $HOME/.vim/project ]; then
-    mkdir ~/.vim/project
-fi
+for dirPath in "${dirPaths[@]}"
+do
+    if [ ! -d $HOME/"$dirPath" ]; then
+        echo "mkdir : $HOME/$dirPath"
+        mkdir $HOME/"$dirPath"
+    fi
+done
+
