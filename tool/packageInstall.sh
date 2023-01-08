23c79bc961750ed6712aabec8afec4c42cfce07f
jyodai <tdkdtk5011@gmail.com>
2023-01-08 14:51:27 +0900

    インストール済みのパッケージはインストールしない

--- tool/packageInstall.sh
+++ tool/packageInstall.sh
@@ -1,5 +1,41 @@
-apt install unzip -y
+#!/bin/bash
 
-curl -fsSL https://deno.land/x/install/install.sh | sh
+main() {
+    installPackages
+}
 
-apt install ripgrep -y
+installPackages() {
+    packages=(
+        "unzip"
+        "ripgrep"
+    )
+
+    for package in "${packages[@]}"
+    do
+        serch=`dpkg -l | grep $package`
+        if [ -n "$serch" ]; then
+            echo "$package"'はインストール済みです'
+            continue
+        fi
+
+
+        echo "$package"'をインストールします'
+        sudo apt install $package -y
+    done
+
+    installDeno
+}
+
+installDeno() {
+    versionInfo=`deno --version`
+    if [[ ! "$versionInfo" == *not\ found* ]]; then
+        echo 'denoはインストール済みです'
+        return
+    fi
+
+    echo 'denoをインストールします'
+    curl -fsSL https://deno.land/x/install/install.sh | sh
+}
+
+
+main
