f7a1aa33cd83242140b5d89a3751b722c7c7e3b7
jyodai <tdkdtk5011@gmail.com>
2023-11-25 06:30:23 +0900

    * sedコマンドをGNUにする

--- git/changeUser.sh
+++ git/changeUser.sh
@@ -1,29 +1,39 @@
+
+
 main() {
     case "$1" in
-      "c")
-        changeCompanyUser
-        ;;
-      "p")
-        changePrivateUser
-        ;;
-      *)
-        echo "undefined";;
+        "c")
+            changeCompanyUser
+            ;;
+        "p")
+            changePrivateUser
+            ;;
+        *)
+            echo "undefined";;
     esac
+}
+
+chooseSedCommand() {
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "gsed"
+    else
+        echo "sed"
+    fi
 }
 
 changeCompanyUser() {
     echo "change company user : $GITHUB_COMPANY_USER"
-    sed -i "s/\(name\s*=\).*$/\1$GITHUB_COMPANY_USER/" $SETTING_PATH/.gitconfig.user
-    sed -i "s/\(email\s*=\).*$/\1$GITHUB_COMPANY_MAIL/" $SETTING_PATH/.gitconfig.user
+    $SED_COMMAND -i "s/\(name\s*=\).*$/\1$GITHUB_COMPANY_USER/" $SETTING_PATH/.gitconfig.user
+    $SED_COMMAND -i "s/\(email\s*=\).*$/\1$GITHUB_COMPANY_MAIL/" $SETTING_PATH/.gitconfig.user
 }
 
 changePrivateUser() {
     echo "change private user : $GITHUB_PRIVATE_USER"
-    sed -i "s/\(name\s*=\).*$/\1$GITHUB_PRIVATE_USER/" $SETTING_PATH/.gitconfig.user
-    sed -i "s/\(email\s*=\).*$/\1$GITHUB_PRIVATE_MAIL/" $SETTING_PATH/.gitconfig.user
+    $SED_COMMAND -i "s/\(name\s*=\).*$/\1$GITHUB_PRIVATE_USER/" $SETTING_PATH/.gitconfig.user
+    $SED_COMMAND -i "s/\(email\s*=\).*$/\1$GITHUB_PRIVATE_MAIL/" $SETTING_PATH/.gitconfig.user
 }
 
-
-
 . $HOME/.env
+SED_COMMAND=$(chooseSedCommand)
 main $1
+
